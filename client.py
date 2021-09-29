152dbc955dd74775fabd2f4558512dffb09fd14c
tuh00755 <tuh00755@temple.edu>
2021-09-28 20:14:21 -0400

    updated variables

--- client.py
+++ client.py
@@ -11,32 +11,32 @@ s.connect(('127.0.0.1', 3000))
 print(s.recv(1024).decode())
 
 # Reading DESkey from DESkey file and defining DESkey
-DESkeyFile = open("key", "r")
-DESkeyString = DESkeyFile.read()
-DESkeyFile.close()
-DESkey = DesKey(DESkeyString.encode('utf-8'))
+desFile = open("key", "r")
+desKeystr = desFile.read()
+desFile.close()
+key = DesKey(desKeystr.encode('utf-8'))
 
 # Reading HMACkey from file and defining HMACkey
-HMACkeyFile = open("hmackey", "r")
-HMACkeyString = HMACkeyFile.read()
-HMACkeyFile.close()
-HMACkey = HMACkeyString.encode('utf-8')
+hmacKey = open("hmackey", "r")
+hmacKeystr = hmacKey.read()
+hmacKey.close()
+byteHmacKey = hmacKeystr.encode('utf-8')
 
 while True:
     message = input("Type message: ")
     messageEncoded = message.encode()
 
     # getting HMAC Digest of message
-    HMACdigest = hmac.new(HMACkey, messageEncoded, hashlib.sha1).hexdigest()
+    HMACdigest = hmac.new(byteHmacKey, messageEncoded, hashlib.sha1).hexdigest()
 
     # Concantenating original message and HMAC digest
     ConcantMessage = message + " " + HMACdigest
 
     # Encrypting the Concantenated Message
-    ConcantCipherText = DESkey.encrypt(ConcantMessage.encode('utf-8'), padding=True)
+    ConcantCipherText = key.encrypt(ConcantMessage.encode('utf-8'), padding=True)
     print("--- Sender Side ---")
-    print("Shared DES key is: ", DESkeyString)
-    print("Shared HMAC key is: ", HMACkeyString)
+    print("Shared DES key is: ", desKeystr)
+    print("Shared HMAC key is: ", hmacKeystr)
     print("Plain message is: ", message)
     print("Sender side HMAC is: ", HMACdigest)
     print("Sent ciphertext is: ", ConcantCipherText.decode('utf-8', 'ignore'))
@@ -52,7 +52,7 @@ while True:
     print("Received ciphertext is: ", rcv.decode('utf-8', 'ignore'))
 
     # Decrypting Concantenated Message
-    decrypted = DESkey.decrypt(rcv, padding=True).decode()
+    decrypted = key.decrypt(rcv, padding=True).decode()
     splitCipher = decrypted.split()
     decryptedHMACDigest = splitCipher[-1]
     rm = splitCipher[:-1]
@@ -61,7 +61,7 @@ while True:
     print("Received HMAC is: " + decryptedHMACDigest)
 
     # Calculating hmac for verification
-    HMACcalc = hmac.new(HMACkey, decryptedMessage.encode(), hashlib.sha1).hexdigest()
+    HMACcalc = hmac.new(byteHmacKey, decryptedMessage.encode(), hashlib.sha1).hexdigest()
     print("Calculated HMAC is: " + HMACcalc)
 
     if (HMACcalc == decryptedHMACDigest):
